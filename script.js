561ed5c9e54b467dfb2e700ad39b0517a8bb0665
Xuanderr <pozdeevk1999@gmail.com>
2023-03-03 16:35:47 +0400

    4th commit

--- script.js
+++ script.js
@@ -1,7 +1,56 @@
 var canvas = document.getElementById("work_space");
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+canvas.width = window.outerWidth;
+canvas.height = window.outerHeight;
 ctx = canvas.getContext("2d");
+var scale = 1;
+//horizontal lines
+for (var i = 0; i <= canvas.height; i += 10) {
+  // начинаем рисовать
+  ctx.beginPath();
+  // ставим начальную точку
+  ctx.moveTo(0, i);
+  // указываем конечную точку для линии
+  ctx.lineTo(canvas.width, i);
+  // рисуем и выводим линию
+  ctx.stroke();
+}
+
+//vertical lines
+for (var i = 0; i <= canvas.width; i += 10) {
+  // начинаем рисовать
+  ctx.beginPath();
+  // ставим начальную точку
+  ctx.moveTo(i, 0);
+  // указываем конечную точку для линии
+  ctx.lineTo(i, canvas.height);
+  // рисуем и выводим линию
+  ctx.stroke();
+}
+
+ctx.strokeStyle = "#876f9e"; // Цвет обводки
+ctx.lineWidth = 6; // Ширина линии
+ctx.beginPath();
+ctx.strokeRect(50, 50, 200, 200);
+ctx.rotate(0.52);
+ctx.strokeRect(50, 50, 200, 200);
+ctx.closePath();
+ctx.scale(2, 2);
+ctx.beginPath();
+ctx.strokeRect(50, 50, 200, 200);
+
+// function moreScale() {
+//   console.log("moreScale");
+//   scale += 0.5;
+//   ctx.scale(scale, scale);
+//   console.log(scale);
+// }
+
+// function lessScale() {
+//   console.log("lessScale");
+//   scale -= 0.5;
+//   ctx.scale(scale, scale);
+//   console.log(scale);
+// }
 
 // var myGridObject = {
 //   canvasWidth: 400, //ширина холста
