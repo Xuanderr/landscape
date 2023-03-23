af0a4541a909456057dca1138e98347980f1f66a
Xuanderr <pozdeevk1999@gmail.com>
2023-03-23 15:15:37 +0400

    StartAddLine

--- main.js
+++ main.js
@@ -17,27 +17,6 @@ window.addEventListener("resize", function () {
   canvas.setHeight(document.getElementById("container").clientHeight);
 });
 
-let circle1 = new fabric.Circle({
-  radius: 50,
-  fill: "red",
-  left: 0,
-});
-let circle2 = new fabric.Circle({
-  radius: 50,
-  fill: "green",
-  left: 100,
-});
-let circle3 = new fabric.Circle({
-  radius: 50,
-  fill: "blue",
-  left: 200,
-});
-let group = new fabric.Group([circle1, circle2, circle3], {
-  left: 200,
-  top: 100,
-});
-canvas.add(group);
-
 canvas.on("mouse:wheel", function (opt) {
   let delta = opt.e.deltaY;
   let zoom = canvas.getZoom();
@@ -48,6 +27,67 @@ canvas.on("mouse:wheel", function (opt) {
   opt.e.preventDefault();
   opt.e.stopPropagation();
 });
+
+const btn = document.getElementById("addBtn");
+btn.addEventListener("click", addLines);
+
+let mouseDown = false;
+function addLines() {
+  canvas.on("mouse:down", startAddLine);
+  canvas.on("mouse:move", startDrawLine);
+  canvas.on("mouse:up", stopDrawingLine);
+  canvas.selection = false;
+}
+
+let line;
+function startAddLine(opt) {
+  mouseDown = true;
+  let pointer = canvas.getPointer(opt.pointer);
+  line = new fabric.Line(
+    [opt.pointer.x, opt.pointer.y, opt.pointer.x, opt.pointer.y],
+    {
+      stroke: "red",
+      strokeWidth: 3,
+    }
+  );
+  canvas.add(line);
+  canvas.requestRenderAll();
+}
+
+function startDrawLine(opt) {
+  if (mouseDown) {
+    line.set({
+      x2: opt.pointer.x,
+      y2: opt.pointer.y,
+    });
+    canvas.requestRenderAll();
+  }
+}
+
+function stopDrawingLine(opt) {
+  mouseDown = false;
+}
+
+// let circle1 = new fabric.Circle({
+//   radius: 50,
+//   fill: "red",
+//   left: 0,
+// });
+// let circle2 = new fabric.Circle({
+//   radius: 50,
+//   fill: "green",
+//   left: 100,
+// });
+// let circle3 = new fabric.Circle({
+//   radius: 50,
+//   fill: "blue",
+//   left: 200,
+// });
+// let group = new fabric.Group([circle1, circle2, circle3], {
+//   left: 200,
+//   top: 100,
+// });
+// canvas.add(group);
 
 // function gridSizeZooming(delta, element) {
 //   let gridZoom = parseInt(element.style.backgroundSize.split(" ")[0]);
